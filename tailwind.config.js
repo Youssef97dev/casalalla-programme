b5e995e06045b876cbdc876d6c8503e71ddb21ad
Youssef97dev <youssefmoussafi97@gmail.com>
2024-12-23 13:55:58 +0100

    casa lalla programming page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        casa_1: "var(--casa-1)",
+        casa_2: "var(--casa-2)",
+        casa_3: "var(--casa-3)",
+        casa_4: "var(--casa-4)",
+        casa_5: "var(--casa-5)",
+        casa_6: "var(--casa-6)",
+        casa_7: "var(--casa-7)",
+        casa_8: "var(--casa-8)",
       },
     },
   },
