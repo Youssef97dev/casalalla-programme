95acd9816e65b06d5dd47a2e2589eebe16cd126b
Youssef97dev <youssefmoussafi97@gmail.com>
2024-12-24 11:13:33 +0100

    desktop version

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -62,7 +62,7 @@ const Events = () => {
       <h1 className="mb-5 text-[34.548px] leading-[42.5631px] lg:text-[60px] lg:leading-[73.92px]">
         Upcoming Events
       </h1>
-      <div className="w-full h-full">
+      <div className="w-full h-full block lg:hidden">
         <Swiper
           modules={[Navigation, Autoplay]}
           autoplay={{
@@ -82,6 +82,51 @@ const Events = () => {
             {events.map((event) => (
               <SwiperSlide key={event.id}>
                 <div className="w-full flex flex-col justify-center items-start gap-2 px-3">
+                  <img
+                    src={`https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/izirylze8wxm7adeaewb`}
+                    className="w-full h-full object-cover"
+                    alt="itemImage"
+                  />
+                  <h1 className="text-3xl">{event.title}</h1>
+                  <h2 className="text-lg mb-3">{event.date_}</h2>
+                  <p>{event.para_1}</p>
+                  <p>{event.para_2}</p>
+                  <p>{event.para_3}</p>
+                  {event.book && (
+                    <Link
+                      href="#"
+                      className="bg-casa_8 text-white px-12 py-5 text-[18px] font-medium mt-3"
+                    >
+                      BOOK
+                    </Link>
+                  )}
+                </div>
+              </SwiperSlide>
+            ))}
+          </div>
+        </Swiper>
+      </div>
+      <div className="w-full h-full hidden lg:block">
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          slidesPerView={4}
+          speed={1400}
+          loop={true}
+          navigation={{
+            nextEl: ".swiper-button-next-ex1",
+            prevEl: ".swiper-button-prev-ex1",
+          }}
+          className="swiper w-full h-full"
+          id="slider1"
+        >
+          <div className="swiper-wrapper">
+            {events.map((event) => (
+              <SwiperSlide key={event.id}>
+                <div className="w-full flex flex-col justify-center items-start gap-2 px-3 lg:px-8">
                   <img
                     src={`https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/izirylze8wxm7adeaewb`}
                     className="w-full h-full object-cover"
